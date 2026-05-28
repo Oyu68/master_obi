72670d603abee0a72593bd8549c4ab9465dfc2f8
Oyunbolor <cerenouunbileg767gmail.com>
2026-05-28 18:07:43 +0800

    searchPage.js дээр json өгөгдөл шүүж харуулсан

---
+++ src/js/components/searchFoodNameSidebar.js
@@ -0,0 +1,23 @@
+export function renderSearchFoodName() {
+  return `    
+    <!-- Үг бичиж хайх -->
+    <p class="menu-label app-menu-label">🔍︎ Search by food name:</p>
+
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input is-primary" type="text" placeholder="Food Name" id="searchTxt"/>
+        <span class="icon is-small is-left">
+          <i class="fa-solid fa-bowl-rice"></i>
+        </span>
+      </p>
+    </div>
+
+    <div class="field">
+      <p class="control">
+        <button class="button is-primary is-fullwidth" type="button" id="searchBtn">
+          Search
+        </button>
+      </p>
+    </div>   
+  `;
+}
