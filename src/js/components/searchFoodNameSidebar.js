a6a484bc9b48ce097fd0f24398a37c1fb7f85c1c
Oyunbolor <cerenouunbileg767gmail.com>
2026-06-04 19:08:55 +0800

    searchPage.js монгол хэлтэй болгов.

--- src/js/components/searchFoodNameSidebar.js
+++ src/js/components/searchFoodNameSidebar.js
@@ -1,11 +1,13 @@
+import { t } from "../i18n/i18n.js";
+//  Үг бичиж хайх
 export function renderSearchFoodName() {
   return `    
     <!-- Үг бичиж хайх -->
-    <p class="menu-label app-menu-label">🔍︎ Search by food name:</p>
+    <p class="menu-label app-menu-label">🔍︎ ${t("sidebar.searchByFoodName")}:</p>
 
     <div class="field">
       <p class="control has-icons-left">
-        <input class="input is-primary" type="text" placeholder="Food Name" id="searchTxt"/>
+        <input class="input is-primary" type="text" placeholder="${t("sidebar.foodName")}" id="searchTxt"/>
         <span class="icon is-small is-left">
           <i class="fa-solid fa-bowl-rice"></i>
         </span>
@@ -15,7 +17,7 @@ export function renderSearchFoodName() {
     <div class="field">
       <p class="control">
         <button class="button is-primary is-fullwidth" type="button" id="searchBtn">
-          Search
+          ${t("sidebar.searchButton")}
         </button>
       </p>
     </div>   
